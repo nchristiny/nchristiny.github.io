e7274e09585d77c4994cf38e5163130b81df7a91
nchristiny <icarus747@gmail.com>
2015-12-13 04:25:17 -0600

    Adjust footer settings

---
+++ public/js/application.js
@@ -0,0 +1,10 @@
+// A script to open new tab/window for external links
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   }
+}
+
+// TODO: make this have some semblance of functionality
