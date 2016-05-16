222c792a77eb8c2700aa55fc5e5e5dc5047fc9ab
Nick Christiny <icarus747@gmail.com>
2016-05-16 14:22:56 -0500

    Style comment form and comments

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,9 +1,31 @@
-// var ref = new Firebase("https://jekyll-comments.firebaseIO.com/");
-// var postRef = ref.child(slugify(window.location.pathname));
+$(function() {
+  var ref = new Firebase("https://jekyll-comments.firebaseio.com/"),
+    postRef = ref.child(slugify(window.location.pathname));
 
-// function slugify(text) {
-//   return text.toString().toLowerCase().trim()
-//     .replace(/&/g, '-and-')
-//     .replace(/[\s\W-]+/g, '-')
-//     .replace(/[^a-zA-Z0-9-_]+/g,'');
-// }
+    postRef.on("child_added", function(snapshot) {
+      var newPost = snapshot.val();
+      $(".comments").prepend('<div class="comment">' +
+        '<h4 class="comment-name">' + newPost.name + '</h4>' +
+        '<div class="comment-profile-image"><img src="http://www.gravatar.com/avatar/' + newPost.md5Email + '?s=100&d=retro"/></div> ' +
+        '<span class="comment-date">' + moment(newPost.postedAt).fromNow() + '</span><p class="comment-text">' + newPost.message  + '</p></div>');
+    });
+
+    $("#comment").submit(function() {
+      postRef.push().set({
+        name: $("#name").val(),
+        message: $("#message").val(),
+        md5Email: md5($("#email").val()),
+        postedAt: Firebase.ServerValue.TIMESTAMP
+      });
+
+      $("input[type=text], textarea").val("");
+      return false;
+    });
+});
+
+function slugify(text) {
+  return text.toString().toLowerCase().trim()
+    .replace(/&/g, '-and-')
+    .replace(/[\s\W-]+/g, '-')
+    .replace(/[^a-zA-Z0-9-_]+/g,'');
+}
