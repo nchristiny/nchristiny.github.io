f6461ac2f48155c2d5dbbe7e02f6c1920f394f1c
Nick Christiny <icarus747@gmail.com>
2016-05-16 13:23:15 -0500

    Add firebase and comment support scripts

---
+++ public/js/blog.js
@@ -0,0 +1,9 @@
+// var ref = new Firebase("https://jekyll-comments.firebaseIO.com/");
+// var postRef = ref.child(slugify(window.location.pathname));
+
+// function slugify(text) {
+//   return text.toString().toLowerCase().trim()
+//     .replace(/&/g, '-and-')
+//     .replace(/[\s\W-]+/g, '-')
+//     .replace(/[^a-zA-Z0-9-_]+/g,'');
+// }
