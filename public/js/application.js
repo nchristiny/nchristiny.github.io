70a6c94d67a3edd0ffaab0f3e17c31fc86689f95
nchristiny <icarus747@gmail.com>
2016-12-14 19:57:12 -0600

    Add vanilla Js DOMContentLoaded event

--- public/js/application.js
+++ public/js/application.js
@@ -2,10 +2,18 @@
 * Open external links in new tabs automatically
 */
 
-var links = document.links;
+var domReady = function(callback) {
+// Enable jQuery's $(document).ready() in Vanilla Js
+    document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+};
 
-for (var i = 0; i < links.length; i++) {
-  if (links[i].hostname != window.location.hostname) {
-    links[i].target = '_blank';
+domReady(function() {
+
+  var links = document.links;
+
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].hostname != window.location.hostname) {
+      links[i].target = '_blank';
+    }
   }
-}
+});
